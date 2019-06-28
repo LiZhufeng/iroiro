190612c6767eb19b3c004e68aa006f1d6d82b114
LiFeng <459559238@qq.com>
2019-06-28 15:01:21 +0800

    Update MetaSpaceTest.java

--- JVM虚拟机/MetaSpaceTest.java
+++ JVM虚拟机/MetaSpaceTest.java
@@ -1,4 +1,4 @@
-package com.lifeng.springbootdemo.basic;
+package Jvm;
 
 import java.io.File;
 import java.net.URL;
@@ -8,9 +8,27 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Created by l00426746 on 2019/6/28.
+ */
 public class MetaSpaceTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+//        String s = new String("1");
+//        s.intern();
+//        String s2 = "1";
+//        System.out.println(s == s2);
+
+        String s3 = new String("1") + new String("1");
+        //String s3 = "1" + "1";
+//        String s5 = s3.intern();
+        String s4 = "11";
+        System.out.println(s3 == s4);
+        System.out.println(s3.equals(s4));
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(i);
+            Thread.sleep(1);
+        }
 //        testOutMemoryOfMetaSpace();
         testStringInternExceed();
     }
@@ -36,20 +54,26 @@ public class MetaSpaceTest {
 
     public static void testStringInternExceed() {
         String originStr = "这是用于测试的字符串";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10000; i++) {
+            sb.append(originStr);
+        }
+        originStr = sb.toString();
 //        List<String> list = new ArrayList<>();
         Map<String, Long> map = new ConcurrentHashMap<>();
         long index = 0;
         try {
             while (true) {
                 String str = originStr + index;
-//                list.add(str.intern());
-                map.put(str.intern(), index);
+                str = str.intern();
+                map.put(str, index);
+                index++;
+                System.out.println("map的大小为：" + map.size());
             }
         }
         catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 
 }
